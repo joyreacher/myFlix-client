786702150c98e99972b294dcb90519c1146c4025
Brian Thomas <btdeveloper125@gmail.com>
2021-07-26 14:17:08 -0600

    This commit imports styles and creates basic class component

--- src/index.jsx
+++ src/index.jsx
@@ -1,2 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//styles
+import './index.scss';
+
+class MyFlixApplication extends React.Component{
+  render(){
+    return(
+      <div className="my-flix">
+        <div Good morning/>
+      </div>
+    )
+  }
+}
