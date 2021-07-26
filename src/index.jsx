036191c4f696be545625fbe05398d64cc86e3279
Brian Thomas <btdeveloper125@gmail.com>
2021-07-26 14:28:29 -0600

    This commit fixes document selector

--- src/index.jsx
+++ src/index.jsx
@@ -7,13 +7,13 @@ class MyFlixApplication extends React.Component{
   render(){
     return(
       <div className="my-flix">
-        <div Good morning/>
+        <div>Good morning</div>
       </div>
     )
   }
 }
 
 //Finds the root of your app
-const container = document.getElementByClassName('app-container')[0];
+const container = document.getElementsByClassName('app-container')[0];
 //Tells React to render your app in the root DOM el
 ReactDOM.render(React.createElement(MyFlixApplication), container);
