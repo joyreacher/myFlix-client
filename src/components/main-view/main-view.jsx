5eaa457cbe3407ff9231f2fb52bfdb87fdebbca5
Brian Thomas <btdeveloper125@gmail.com>
2021-07-27 12:41:51 -0600

    This commit sets up an for MovieCard to use event listener to display movie info

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -9,13 +9,13 @@ export default class MainView extends React.Component{
       selectedMovie: null,
       movies:[
         {
-          _id: 1, Title: "Inception", description: 'Inception description', ImagePath: 'inception.png'
+          _id: 1, Title: "Inception", Description: 'Inception description', ImagePath: 'inception.png'
         },
         {
-          _id: 2, Title: "The Shawshank Redemption", description: 'Shawshank Redemption description', ImagePath: 'shawshankredemption.png'
+          _id: 2, Title: "The Shawshank Redemption", Description: 'Shawshank Redemption description', ImagePath: 'shawshankredemption.png'
         },
         {
-          _id: 3, Title: "Gladiator", description: 'Gladiator description', ImagePath: 'gladiator.png'
+          _id: 3, Title: "Gladiator", Description: 'Gladiator description', ImagePath: 'gladiator.png'
         },
       ]
     }
@@ -26,7 +26,7 @@ export default class MainView extends React.Component{
     if(movies.length === 0)return <div className="main-view">The list is empty!</div>;
     return(
       <div className='main-view'>
-        {movies.map(movie => <MovieCard  key={movie._id} movie={movie} onMovieClick={(newSelectedMovie)=>{this.state.selectedMovie = newSelectedMovie}}/>)}
+        {movies.map(movie => <MovieCard  key={movie._id} movie={movie} onMovieClick={(newSelectedMovie)=>{this.setState({selectedMovie: newSelectedMovie})}}/>)}
       </div>
     );
   }
