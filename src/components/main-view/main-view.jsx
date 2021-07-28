d39b2c932c4b0cde1c2eb5f1ea97b2e4963a7434
Brian Thomas <btdeveloper125@gmail.com>
2021-07-28 09:14:36 -0600

    This commit runs standard js lint

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,43 +1,46 @@
-import React from 'react';
+import React from 'react'
 import MovieCard from '../movie-card/movie-card'
 import MovieView from '../movie-view/movie-view'
 
-export default class MainView extends React.Component{
-  constructor(){
-    super();
+export default class MainView extends React.Component {
+  constructor () {
+    super()
     this.state = {
       selectedMovie: null,
-      movies:[
+      movies: [
         {
-          _id: 1, Title: "Inception", Description: 'Inception description', ImagePath: 'inception.png'
+          _id: 1, Title: 'Inception', Description: 'Inception description', ImagePath: 'inception.png'
         },
         {
-          _id: 2, Title: "The Shawshank Redemption", Description: 'Shawshank Redemption description', ImagePath: 'shawshankredemption.png'
+          _id: 2, Title: 'The Shawshank Redemption', Description: 'Shawshank Redemption description', ImagePath: 'shawshankredemption.png'
         },
         {
-          _id: 3, Title: "Gladiator", Description: 'Gladiator description', ImagePath: 'gladiator.png'
-        },
+          _id: 3, Title: 'Gladiator', Description: 'Gladiator description', ImagePath: 'gladiator.png'
+        }
       ]
     }
   }
-  setSelectedMovie(newSelectedMovie){
+
+  setSelectedMovie (newSelectedMovie) {
     this.setState({
       selectedMovie: newSelectedMovie
     })
   }
-  render(){
+
+  render () {
     const { movies, selectedMovie } = this.state
-    if(movies.length === 0)return <div className="main-view">The list is empty!</div>;
-    return(
+    if (movies.length === 0) return <div className='main-view'>The list is empty!</div>
+    return (
       <div className='main-view'>
         {
-          selectedMovie ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie =>{this.setSelectedMovie(newSelectedMovie);}}/>
-          :
-          movies.map(movie => (
-            <MovieCard key={movie._id} movie={movie} onMovieClick={movie=>{ this.setSelectedMovie(movie)}} />
-          ))
+          selectedMovie
+          // onBackClick(null) -- will run the movies.map condition and display the MovieCard
+            ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie) }} />
+            : movies.map(movie => (
+              <MovieCard key={movie._id} movie={movie} onMovieClick={movie => { this.setSelectedMovie(movie) }} />
+            ))
         }
       </div>
-    );
+    )
   }
 }
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
+import React from 'react'
 
 class MovieView extends React.Component {
-  render() {
+  render () {
     const { movie, onBackClick } = this.props
     return (
-      <div className="movie-view">
-        <div className="movie-poster">
+      <div className='movie-view'>
+        <div className='movie-poster'>
           <img src={movie.ImagePath} />
         </div>
-        <div className="movie-title">
-          <span className="label">Title: </span>
-          <span className="value">{movie.Title}</span>
+        <div className='movie-title'>
+          <span className='label'>Title: </span>
+          <span className='value'>{movie.Title}</span>
         </div>
-        <div className="movie-description">
-          <span className="label">Description: </span>
-          <span className="value">{movie.Description}</span>
+        <div className='movie-description'>
+          <span className='label'>Description: </span>
+          <span className='value'>{movie.Description}</span>
         </div>
-        <button onClick={()=>onBackClick(null)}>Back</button>
+        <button onClick={() => onBackClick(null)}>Back</button>
       </div>
-    );
+    )
   }
 }
 
-export default MovieView;
+export default MovieView
