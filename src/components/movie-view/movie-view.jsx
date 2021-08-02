a258c5672c68d8c6184a684b341c4fe7fd056057
Brian Thomas <btdeveloper125@gmail.com>
2021-08-02 09:41:01 -0600

    feat: This commit adds error boundary and BS styles

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import ErrorBoundary from '../ErrorBoundary'
+
+// Bootstrap
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 class MovieView extends React.Component {
   // refer to the callback function when adding and removing the listener
   keypressCallback (event) {
@@ -19,20 +24,24 @@ class MovieView extends React.Component {
   render () {
     const { movie, onBackClick } = this.props
     return (
-      <div className='movie-view'>
-        <div className='movie-poster'>
-          <img src={movie.ImagePath} />
-        </div>
-        <div className='movie-title'>
-          <span className='label'>Title: </span>
-          <span className='value'>{movie.Title}</span>
-        </div>
-        <div className='movie-description'>
-          <span className='label'>Description: </span>
-          <span className='value'>{movie.Description}</span>
-        </div>
-        <button onClick={() => onBackClick(null)}>Back</button>
-      </div>
+      <ErrorBoundary>
+        <Row className='movie-view'>
+          <Col className='movie-poster'>
+            <img src={movie.ImagePath} />
+          </Col>
+          <Col>
+            <div className='movie-title'>
+              <span className='label'>Title: </span>
+              <span className='value'>{movie.Title}</span>
+            </div>
+            <div className='movie-description'>
+              <span className='label'>Description: </span>
+              <span className='value'>{movie.Description}</span>
+            </div>
+            <button onClick={() => onBackClick(null)}>Back</button>
+          </Col>
+        </Row>
+      </ErrorBoundary>
     )
   }
 }
