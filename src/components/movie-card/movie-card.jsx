b3f19470cad1b84f47af2ba4d27b65de6f0f437f
Brian Thomas <btdeveloper125@gmail.com>
2021-08-03 15:15:05 -0600

    refactor: This commit builds the basic layout of the mainpage

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Row, Col, Container, Card, Button, CardGroup, CardColumns } from 'react-bootstrap'
+import { Row, Col, Container, Card, Button, CardGroup, Image } from 'react-bootstrap'
 
 // custom styles
 import './movie-card.scss'
@@ -9,22 +9,22 @@ export default class MovieCard extends React.Component {
     const { movies, onMovieClick } = this.props
     return (
       <Container className='d-flex flex-column'>
-        <h1 >Featured</h1>
-        <Row lg={6} className='align-items-center'>
+        <div className='section__start'>
+          <h1 className='display-1'>Featured</h1>
+        </div>
+        <Row xs={2} sm={2} md={4} lg={6} className='align-items-center'>
           {
           movies.map(movie => {
             if (movie.Featured === true) {
               return (
                 <>
-                <CardGroup>
-                  <Col lg={12}>
-                      <Card key={movie._id} className='m-2 bg-light'>
+                <CardGroup className='my-5'>
+                  <Col xs={12} sm={12} md={12} lg={12}>
+                      <Card key={movie._id} className='m-2 bg-light mb-sm-1'>
                         <Card.Header>{movie.Title}</Card.Header>
                         <Card.Img variant='top' src={movie.ImagePath} />
                           <Card.Body className='p-2'>
-                            <Card.Text className='text-truncate'>
-                              {movie.Description}
-                            </Card.Text>
+                            <Card.Text className='text-truncate'>{movie.Description}</Card.Text>
                           </Card.Body>
                         <Button className='btn btn-dark' onClick={() => onMovieClick(movie)}>Open</Button>
                       </Card>
@@ -36,24 +36,23 @@ export default class MovieCard extends React.Component {
           })
         }
         </Row>
-        <h1 className='ms-2'>Browse</h1>
+        <div className='section__start'>
+          <h1 className='ps-2 display-1'>Browse</h1>
+        </div>
         <Row>
-        
         {
           movies.map(movie => {
             if (!movie.Featured) {
               return (
-                <Row className='justify-content-between'>
-                  <Row className='mb-5 align-items-center'>
-                    <Col lg={6}>
-                      <img className='min-vh-100 ms-2 mt-5' src={movie.ImagePath} />
-                    </Col>
-                    <Col lg={6}>
-                      <h2 className='display-2'>{movie.Title}</h2>
-                      <p>{movie.Description}</p>
-                      <Button className='btn btn-dark' onClick={() => onMovieClick(movie)} variant='button'>Open</Button>
-                    </Col>
-                  </Row>
+                <Row md={2} className='mb-5 align-items-center justify-content-between' >
+                  <Col sm={3} md={12} lg={3}>
+                    <Image className='ms-2 mt-5' src={movie.ImagePath} />
+                  </Col>
+                  <Col lg={6}>
+                    <h2 className='display-2'>{movie.Title}</h2>
+                    <p>{movie.Description}</p>
+                    <Button className='btn btn-dark' onClick={() => onMovieClick(movie)} variant='button'>Open</Button>
+                  </Col>
                 </Row>
               )
             }
