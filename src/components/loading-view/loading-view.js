9535e06d24d39b93475bc311cb22a9e053dcc96e
Brian Thomas <btdeveloper125@gmail.com>
2021-08-04 15:06:17 -0600

    feat: This commit adds gsap loading animation and react-bs spinner

--- src/components/loading-view/loading-view.js
+++ src/components/loading-view/loading-view.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 // bootstrap
 import { Container, Spinner, Row } from 'react-bootstrap'
@@ -6,11 +6,16 @@ import { Container, Spinner, Row } from 'react-bootstrap'
 import './loading-view.scss'
 
 function Loading () {
+  useEffect(()=>{
+    let LoadingText = document.querySelector('.loading__text');
+    let tl = gsap.timeline({yoyo: true})
+    tl.from(LoadingText, { duration: 1.2, opacity: 0, yoyo: true, repeat: -1})
+  }, [])
   return (
     <Container>
       <Row className='d-grid'>
+        <h1 className='loading__text'>Loading</h1>
         <Spinner animation='border' />
-        <h1 className='animation__loading'>Loading</h1>
       </Row>
     </Container>
   )
