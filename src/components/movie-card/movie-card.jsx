cad0c0e8589319af86d7b0ca540e8241953a78ab
Brian Thomas <btdeveloper125@gmail.com>
2021-08-06 13:23:59 -0600

    refactor: This commit updates the not featured movies

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom'
 
 // custom styles
 import './movie-card.scss'
+import NotFeatured from '../not-featured/not-featured'
 export default class MovieCard extends React.Component {
   render () {
     const { movies } = this.props
@@ -41,26 +42,7 @@ export default class MovieCard extends React.Component {
           <h1 className='display-1 fs-1 mt-5'>Browse</h1>
         </div>
         <Row>
-          {
-          movies.map(movie => {
-            if (!movie.Featured) {
-              return (
-                <Row className='mb-5 justify-content-between align-items-center d-flex' key={`not-featured-${movie._id}`}>
-                  <Col xs={12} sm={12} md={4} lg={3} className='d-sm-flex justify-content-sm-center px-4 my-4'>
-                    <Image className='mt-5' src={movie.ImagePath} />
-                  </Col>
-                  <Col xs={12} sm={12} md={6} lg={8} className='text-sm-center text-md-start'>
-                    <h2 className='fs-1'>{movie.Title}</h2>
-                    <p className='text-truncate fs-4'>{movie.Description}</p>
-                    <Link to={`/movies/${movie._id}`}>
-                      <Button className='btn btn-dark'>Open</Button>
-                    </Link>
-                  </Col>
-                </Row>
-              )
-            }
-          })
-        }
+          <NotFeatured movies={movies} />
         </Row>
       </Container>
     )
