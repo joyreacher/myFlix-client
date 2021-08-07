85f52ea9492e4dae4685c1235eb5758dcd9b545e
Brian Thomas <btdeveloper125@gmail.com>
2021-08-07 13:30:00 -0600

    feat: This commit adds logged out state

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { LoginView } from '../login-view/login-view'
 // Bootstrap
 import { Navbar, Container, Row, Button } from 'react-bootstrap'
 
 // Custom styles
 import './navbar.scss'
-function Nav ({ onLoggedIn, user }) {
-  console.log(user)
+
+function Nav ({ onLogOutClick, user }) {
+  if(!user) return (
+    <Navbar className='mt-2 mb-5'>
+      <Container>
+        <Navbar.Brand>
+          <Row className='d-flex justify-content-start align-items-center'>
+            <Link className='home-link' to='/'>
+              <span className='cinema'>Cinema</span>
+              <span className='barn'>Barn</span>
+            </Link>
+          </Row>
+        </Navbar.Brand>
+      </Container>
+    </Navbar>
+  )
   return (
     <Navbar className='mt-2 mb-5'>
       <Container>
@@ -19,12 +34,12 @@ function Nav ({ onLoggedIn, user }) {
           </Row>
         </Navbar.Brand>
         <Navbar.Collapse className='justify-content-end'>
-          <Navbar.Text onClick={() => onLoggedIn()}>
+          <Navbar.Text>
             Signed in as: <Link to={`/user/${user}`}>{user}</Link>
           </Navbar.Text>
         </Navbar.Collapse>
         <Navbar.Collapse className='justify-content-end'>
-          <Navbar.Text onClick={() => onLoggedIn()}>
+          <Navbar.Text onClick={()=>onLogOutClick()}>
             <Button className='btn bg-dark'>Logout</Button>
           </Navbar.Text>
         </Navbar.Collapse>
