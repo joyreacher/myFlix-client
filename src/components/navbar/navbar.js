6cb8eef0293e357f338484f4b260758c87db00ee
Brian Thomas <btdeveloper125@gmail.com>
2021-08-07 13:01:39 -0600

    feat: This commit adds the navbar layout

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -5,18 +5,29 @@ import { Navbar, Container, Row } from 'react-bootstrap'
 
 // Custom styles
 import './navbar.scss'
-function Nav () {
+function Nav ({ onLoggedIn, user }) {
+  console.log(user)
   return (
     <Navbar className='mt-2 mb-5'>
       <Container>
         <Navbar.Brand>
           <Row className='d-flex justify-content-start align-items-center'>
-            <Link className='home-link' to={'/'}>
+            <Link className='home-link' to='/'>
               <span className='cinema'>Cinema</span>
               <span className='barn'>Barn</span>
             </Link>
           </Row>
         </Navbar.Brand>
+        <Navbar.Collapse className='justify-content-end'>
+          <Navbar.Text onClick={() => onLoggedIn()}>
+            Signed in as: {user}
+          </Navbar.Text>
+        </Navbar.Collapse>
+        <Navbar.Collapse className='justify-content-end'>
+          <Navbar.Text onClick={() => onLoggedIn()}>
+            Logout
+          </Navbar.Text>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   )
