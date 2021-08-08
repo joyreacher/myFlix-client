29ba17c09dcdb6d60b3d4717a7ae7b3130b8f928
Brian Thomas <btdeveloper125@gmail.com>
2021-08-08 10:37:45 -0600

    feat: This commit updates profile view

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import { Container } from 'react-bootstrap'
+import Loading from '../loading-view/loading-view'
+import { Row, Col, Container } from 'react-bootstrap'
 export default function ProfileView ({ user }) {
   const [list, setList] = useState([])
   useEffect(() => {
@@ -8,16 +9,35 @@ export default function ProfileView ({ user }) {
     axios.get(`https://cinema-barn.herokuapp.com/user/${user}`, {
       headers: { Authorization: `Bearer ${accessToken}` }
     }).then(res => {
-      console.log(res.data)
-      setList(res.data)
-      return list
+      return setList(res.data)
     }).catch(function (error) {
       console.log(error)
     })
   }, [])
+  console.log(list.favorite_movies)
+  if (list.length === 0) return <Loading />
   return (
-    <Container>
-      <h1>{list.username}</h1>
+    <Container className=''>
+      <h1 className='my-5 bg-dark text-light d-inline-block'>{list.username}'s Profile</h1>
+      <div className='d-flex justify-content-center p-2 my-5'>
+        <Row lg={12}>
+          <Col>
+            <img src='' alt='Image goes here' />
+            <p>{list.username}</p>
+            {
+              list.favorite_movies.length === 0
+                ? <p>You have no moves saved.</p>
+                : list.favorite_movies.map(movie => {
+                  return <h1 key={movie._id}>{movie.title}</h1>
+                })
+            }
+          </Col>
+          <Col lg={2}>
+            <p className='fs-6'>{list.email}</p>
+            <p className='fs-6'>{list.birthday}</p>
+          </Col>
+        </Row>
+      </div>
     </Container>
   )
 }
