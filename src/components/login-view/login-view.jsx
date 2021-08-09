18e6f80d94146c3a3eb6ccecaf0629aa2dc820df
Brian Thomas <btdeveloper125@gmail.com>
2021-08-09 10:51:31 -0600

    refactor: This commit adds validation to the console

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -11,26 +11,48 @@ import Form from 'react-bootstrap/Form'
 import FloatingLabel from 'react-bootstrap/FloatingLabel'
 import Button from 'react-bootstrap/Button'
 export function LoginView (props) {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
+  // const [username, setUsername] = useState('')
+  // const [password, setPassword] = useState('')
+  const [username, setUsername] = useState({
+    value: "",
+    placeholder: 'Username',
+    valid: true,
+    typeMismatch: false,
+    errMsg: null
+  })
+  const [password, setPassword] = useState({
+    value: "",
+    placeholder: 'Password',
+    valid: true,
+    typeMismatch: false,
+    errMsg: null
+  })
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    axios.post('https://cinema-barn.herokuapp.com/login', {
-      Username: username,
-      Password: password
-    }).then(res => {
-      const data = res.data
-      props.onLoggedIn(data)
-    }).catch(e => {
-      console.log('no such user')
-    })
+    if (!password.value) {
+      setUsername({ errMsg: 'Enter your password' })
+      console.log(username)
+    } if (!username.value) {
+      setUsername({ errMsg: 'Enter a username' })
+      console.log(username)
+    } else if (username.value && password.value) {
+      axios.post('https://cinema-barn.herokuapp.com/login', {
+        Username: username.value,
+        Password: password.value
+      }).then(res => {
+        const data = res.data
+        props.onLoggedIn(data)
+      }).catch(e => {
+        console.log('no such user')
+      })
+    }
   }
 
   return (
     <Row className='justify-content-center min-vh-100 align-items-center'>
       <Col xs={10} sm={10} md={7} lg={6} xl={7}>
-        <Form className='login__form jumbotron p-5' onSubmit={handleSubmit} noValidate>
+        <Form className='login__form jumbotron p-5' onSubmit={handleSubmit}>
           <Form.Group>
             <Form.Label column='sm'>
               <h1 className='fs-1 display-1'>Sign in</h1>
@@ -38,14 +60,15 @@ export function LoginView (props) {
           </Form.Group>
           <Form.Group>
             <FloatingLabel label='Username' controlId='floatingInput'>
-              <Form.Control placeholder='Username' type='text' value={username} onChange={e => setUsername(e.target.value)} />
+              <Form.Control name='username' placeholder='Username' type='text' value={username.value} onChange={e => setUsername({ value: e.target.value })} />
             </FloatingLabel>
             <FloatingLabel label='Password' controlId='floatingInput'>
-              <Form.Control placeholder='Password' type='password' value={password} onChange={e => setPassword(e.target.value)} />
+              <Form.Control name='pass' placeholder='Password' type='password' value={password.value} onChange={e => setPassword({ value: e.target.value })} />
             </FloatingLabel>
-            <Button variant='success' type='submit' onClick={handleSubmit}>
+            <Form.Control type='submit' value='submit'/>
+            {/* <Button variant='success' type='submit' onClick={handleSubmit}>
               Submit
-            </Button>
+            </Button> */}
           </Form.Group>
           <Form.Group className='d-flex justify-content-start mt-5'>
             <p className='me-2 text-start'>Dont have an account?</p>
