0112207f7fefcd6b0a90c79db23eab8c774c407d
Brian Thomas <btdeveloper125@gmail.com>
2021-08-19 09:50:54 -0600

    style: This commit adds task comments/tips

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,6 +1,19 @@
 import { combineReducers } from 'redux'
 import { SET_FILTER, SET_MOVIES } from '../actions/actions'
 
+/**
+ *
+  It’s handy here to give a default value to the state parameter (state = '', action). 
+  If state were to be undefined and the action out of scope for a reducer (in the visibilityFilter reducer, 
+  if the action wasn’t SET_FILTER),
+  the reducer would return whatever it was passed as the visibilityFilter state—in this case, an empty string ''.
+
+  -a reducer must always return a state even if there have been no changes
+  -reducers should never mutate the state, they should create a new instance of the state to be returned
+    TO ADD A MOVIE TO A LIST
+    --copy the movies array then add to it, then return the new array
+ */
+
 function visibilityFilter (state = '', action) {
   switch (action.type) {
     case SET_FILTER:
@@ -19,7 +32,7 @@ function movies (state = [], action) {
       return state
   }
 }
-//? COMBINE REDUCERS ABOVE
+// ? COMBINE REDUCERS ABOVE
 function moviesApp (state = {}, action) {
   return {
     visibilityFilter: visibilityFilter(state.visibilityFilter, action),
