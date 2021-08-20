599b4903f9fc25a08658bd8d6ea9df869943f5e2
Brian Thomas <btdeveloper125@gmail.com>
2021-08-19 18:57:27 -0600

    refactor: This commit adds footer links

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,26 +1,26 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 import { Navbar, Container, Col } from 'react-bootstrap'
 
 import './footer.scss'
 
-function Footer () {
+function Footer ({ user }) {
+  const path = '/user/' + user
   return (
     <Navbar className='footer'>
       <Container>
         <Col className='footer__col d-flex flex-column align-items-sm-center ps-5' sm={6} md={6} lg={6}>
           <div>
             <h3 className='fs-3'>Cinema Barn</h3>
-            <p>API Documentation</p>
-            <p>Github (Client)</p>
-            <p>Github (API)</p>
+            <a href='https://github.com/joyreacher/movie_api#readme'>API Documentation</a>
+            <a href='https://github.com/joyreacher/myFlix-client'>Github (Client)</a>
+            <a href='https://github.com/joyreacher/movie_api'>Github (API)</a>
           </div>
         </Col>
         <Col className='footer__col d-flex flex-column align-items-sm-center pe-5' sm={6} md={6} lg={6}>
           <div>
             <h3 className='fs-3'>My Account</h3>
-            <p>My Profile</p>
-            <p>My Movies</p>
+            <Link to={path}>My Profile</Link>
           </div>
         </Col>
       </Container>
