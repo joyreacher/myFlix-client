5a2f9d5916c867c13cdad348dad8298b70a5cd9f
Brian Thomas <btdeveloper125@gmail.com>
2021-08-23 11:08:20 -0600

    feat: Rename the card list to search list

--- src/components/card-list/search-list.jsx
+++ src/components/card-list/search-list.jsx
@@ -3,14 +3,16 @@ import { Link } from 'react-router-dom'
 import { Row, Col, Button, Image } from 'react-bootstrap'
 
 const cardList = (props) => {
+  console.log(props.movie.Featured)
   return (
     <Row className='mb-5 justify-content-between align-items-center d-flex' key={`not-featured-${props.movie._id}`}>
-      <Col xs={12} sm={12} md={4} lg={3} className='d-sm-flex justify-content-sm-center px-4 my-4'>
-        <Image className='mt-5' src={props.movie.ImagePath} />
+      <h2>Search Results</h2>
+      <Col xs={12} sm={12} md={4} lg={4} className='my-4'>
+        <Image className='mt-5 w-100 h-100' src={props.movie.ImagePath} />
       </Col>
-      <Col xs={12} sm={12} md={6} lg={8} className='text-sm-center text-md-start'>
+      <Col xs={12} sm={12} md={6} lg={6} className='text-sm-center text-md-start'>
         <h2 className='fs-1'>{props.movie.Title}</h2>
-        <p className='text-truncate fs-4'>{props.movie.Description}</p>
+        <p className='fs-6'>{props.movie.Description}</p>
         <Link to={`/movies/${props.movie._id}`}>
           <Button className='btn btn-dark'>Open</Button>
         </Link>
