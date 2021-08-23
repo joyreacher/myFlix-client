de6d0601db4feaaa0e8f4325df116325ce7a09db
Brian Thomas <btdeveloper125@gmail.com>
2021-08-23 10:52:39 -0600

    feat: This commit adds the feat/not-feat components with filter functionality

--- src/components/movies-list/movies-list.jsx
+++ src/components/movies-list/movies-list.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-import { Col, Container } from 'react-bootstrap'
+import { Col, Container, Row, Image, CardGroup } from 'react-bootstrap'
 import { connect } from 'react-redux'
 
 import VisibilityFilterInput from '../visibility-filter-input/visibility-filter-input'
 import CardList from '../card-list/card-list'
+import NotFeatured from '../not-featured/not-featured'
+import Featured from '../featured/featured'
 
 const mapStateToProps = state => {
   const { visibilityFilter } = state
@@ -20,14 +22,27 @@ function MoviesList (props) {
   if (!movies) return <div className='main-view'>wtf</div>
   return (
     <Container>
-      <Col lg={6}>
-        <VisibilityFilterInput visibilityFilter={visibilityFilter} />
-      </Col>
-      {filteredMovies.map(m => (
-        <Col key={m._id}>
-          <CardList movie={m} />
+      <Row>
+        <Col>
+          <p>Search for a movie</p>
         </Col>
-      ))}
+        <Col lg={9}>
+          <VisibilityFilterInput visibilityFilter={visibilityFilter} />
+        </Col>
+      </Row>
+      {visibilityFilter ? filteredMovies.map(m => { return <Col key={m._id}><CardList movie={m} /></Col> }) : ''}
+      <Row>
+        <Col>
+          <h2>Featured</h2>
+          <CardGroup>
+            <Featured movies={movies}/>
+          </CardGroup>
+        </Col>
+        <Col>
+          <h2>Browse</h2>
+          <NotFeatured movies={movies}/>
+        </Col>
+      </Row>
     </Container>
   )
 }
