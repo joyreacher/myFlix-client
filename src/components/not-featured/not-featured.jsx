41e31f8b69dc2b37974d6984f3a2180383ea0712
Brian Thomas <btdeveloper125@gmail.com>
2021-08-31 08:19:43 -0600

    refactor Thi socmmit updates the not featured movie cards

--- src/components/not-featured/not-featured.jsx
+++ src/components/not-featured/not-featured.jsx
@@ -10,14 +10,21 @@ function NotFeatured ({ movies }) {
           return (
               <Row className='mb-5 justify-content-between align-items-center d-flex' key={`not-featured-${movie._id}`}>
                 <Col xs={12} sm={12} md={4} lg={3} className='d-sm-flex justify-content-sm-center px-4 my-4'>
-                  <Image className='mt-5' src={movie.ImagePath} />
+                  <Image className='mt-5' src={movie.ImagePath}/>
                 </Col>
                 <Col xs={12} sm={12} md={6} lg={8} className='text-sm-center text-md-start'>
                   <h2 className='fs-1'>{movie.Title}</h2>
                   <p className='text-truncate fs-4'>{movie.Description}</p>
-                  <Link to={`/movies/${movie._id}`}>
-                    <Button className='btn btn-dark'>Open</Button>
-                  </Link>
+                </Col>
+                <Col lg={4}>
+                  <div className=''>
+                    <Link className='my-2 mx-4' to={`/movies/${movie._id}`}>
+                      <Button className='btn btn-dark'>Open</Button>
+                    </Link>
+                    <Link className='my-2 mx-4' to={`/movies/${movie._id}`}>
+                      <Button className='btn btn-dark'>Add to favorites</Button>
+                    </Link>
+                  </div>
                 </Col>
               </Row>
           )
