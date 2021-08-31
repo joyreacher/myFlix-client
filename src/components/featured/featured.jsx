6753b617f697ad49c41ceb5dd8c4ae23fa74026f
Brian Thomas <btdeveloper125@gmail.com>
2021-08-31 12:13:31 -0600

    refactor: This commit adds the BS template

--- src/components/featured/featured.jsx
+++ src/components/featured/featured.jsx
@@ -9,18 +9,32 @@ function Featured ({ movies }) {
       movies.map(movie => {
         if (movie.Featured === true) {
           return (
-            <Col lg={4}>
-              <Card className='m-2 mb-sm-1 py-5 px-2' key={`featured-${movie._id}`}>
-                <Card.Header className='fs-1 my-4'>{movie.Title}</Card.Header>
-                <Link to={`/movies/${movie._id}`}>
-                  <Card.Img variant='top' src={movie.ImagePath} />
-                </Link>
-                <Card.Body className='p-4'>
-                  <Card.Text className='text-truncate'>{movie.Description}</Card.Text>
-                </Card.Body>
-                <Button>Add to favorites</Button>
-              </Card>
-            </Col>
+            <div className='container px-4 px-lg-5 my-5'>
+              <div className='row gx-4 gx-lg-5 align-items-center'>
+                <Col lg={4}>
+                  <img className="card-img-top mb-5 mb-md-0" src={movie.ImagePath} alt='movie poster' />
+                </Col>
+                <Col  md={6} lg={8}>
+                  {/* <div className='small mb-1'>{movie.Genre.Name}</div> */}
+                  <h1 className="display-5 fw-bolder">{movie.Title}</h1>
+                  <div className="fs-5 mb-3">
+                      <span>{movie.Genre.Name}</span>
+                  </div>
+                  <p className="lead">{movie.Description}</p>
+                  <div className="d-flex justify-content-lg-evenly">
+                      {/* <input className="form-control text-center me-3" id="inputQuantity" type="num" value="1" style="max-width: 3rem" readOnly/> */}
+                      <button className="btn btn-outline-dark flex-shrink-0" type="button">
+                          <i className="bi-cart-fill me-1"></i>
+                          Add to favorites
+                      </button>
+                      <button className="btn btn-outline-dark flex-shrink-0" type="button">
+                          <i className="bi-cart-fill me-1"></i>
+                          More
+                      </button>
+                  </div>
+                </Col>
+              </div>
+            </div>
           )
         }
       })
