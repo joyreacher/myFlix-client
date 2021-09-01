7492ea17176fe045d65882a9f63673349503412a
Brian Thomas <btdeveloper125@gmail.com>
2021-09-01 11:01:56 -0600

    refactor: This commit adds unique key prop to search results

--- src/components/card-list/search-list.jsx
+++ src/components/card-list/search-list.jsx
@@ -3,10 +3,8 @@ import { Link } from 'react-router-dom'
 import { Row, Col, Button, Image } from 'react-bootstrap'
 
 const cardList = (props) => {
-  console.log(props.movie.Featured)
   return (
-    <Row className='mb-5 justify-content-between align-items-center d-flex' key={`not-featured-${props.movie._id}`}>
-      <h2>Search Results</h2>
+    <Row className='mb-5 justify-content-between align-items-center d-flex' key={`search-result-${props.movie._id}`}>
       <Col xs={12} sm={12} md={4} lg={4} className='my-4'>
         <Image className='mt-5 w-100 h-100' src={props.movie.ImagePath} />
       </Col>
