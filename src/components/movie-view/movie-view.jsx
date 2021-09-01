e24fdb1ab8cada562106c8abd98fc3d84795de82
Brian Thomas <btdeveloper125@gmail.com>
2021-09-01 11:57:56 -0600

    refactor: This commit updates the movie view styles

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
-import ErrorBoundary from '../ErrorBoundary'
 
 // Bootstrap
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import Button from 'react-bootstrap/Button'
 import Image from 'react-bootstrap/Image'
 
 // Custom styles
 import './movie-view.scss'
+import { Container } from 'react-bootstrap'
 class MovieView extends React.Component {
   // refer to the callback function when adding and removing the listener
   keypressCallback (event) {
@@ -31,29 +30,28 @@ class MovieView extends React.Component {
     const { movie, onBackClick } = this.props
     // console.log(movie.Genre)
     return (
-      <ErrorBoundary>
-        <Row md={6} lg={6} xl={6} className='my-5 d-flex justify-content-center min-vh-100 align-items-center'>
-          <Col lg={4}>
+      <Container className='my-5'>
+        <Row lg={12} >
+          <Col lg={3} className='my-5'>
             <Image className='shadow-1' src={movie.ImagePath} />
           </Col>
-          <Col lg={6}>
+          <Col lg={8} className='my-4'>
             <h2 className='display-1 fs-1 movie__title'>{movie.Title}</h2>
             <p className='fs-5'>{movie.Director.Name}</p>
             <p className='fs-5'>{movie.Genre.Name}</p>
-            {/* <p className='fs-5'>{movie.Director}</p> */}
             <p className='fs-6'>{movie.Description}</p>
-            <div className='d-flex justify-content-around'>
-              <Link to={`/directors/${movie.Director.Name}`}>
-                <Button className='shadow-1'>Director</Button>
-              </Link>
-              <Link to={`/genres/${movie.Genre.Name}`}>
-                <Button className='shadow-1'>Genre</Button>
-              </Link>
-              <Button className='shadow-1 btn btn-dark fs-1' onClick={() => onBackClick(null)}>Back</Button>
-            </div>
+          </Col>
+          <Col lg={12} className='mb-5 d-flex justify-content-evenly'>
+            <Link to={`/directors/${movie.Director.Name}`}>
+              <button className='shadow-1  btn btn-outline-dark'>Director</button>
+            </Link>
+            <Link to={`/genres/${movie.Genre.Name}`}>
+              <button className='shadow-1  btn btn-outline-dark'>Genre</button>
+            </Link>
+            <button className='shadow-1  btn btn-outline-dark' onClick={() => onBackClick(null)}>Back</button>
           </Col>
         </Row>
-      </ErrorBoundary>
+      </Container>
     )
   }
 }
