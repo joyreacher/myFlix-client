73089bab7f95f445b5420a282f73c912fa46796d
Brian Thomas <btdeveloper125@gmail.com>
2021-09-02 11:44:45 -0600

    feat: This commit updates the footer links

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -12,15 +12,15 @@ function Footer ({ user }) {
         <Col className='footer__col d-flex flex-column align-items-center ps-5' sm={6} md={6} lg={6}>
           <div className='d-flex flex-column'>
             <h3 className='fs-3'>Cinema Barn</h3>
-            <a href='https://github.com/joyreacher/movie_api#readme'>API Documentation</a>
-            <a href='https://github.com/joyreacher/myFlix-client'>Github (Client)</a>
-            <a href='https://github.com/joyreacher/movie_api'>Github (API)</a>
+            <a className='footer-link' target='_blank' href='https://github.com/joyreacher/movie_api#readme'>API Documentation</a>
+            <a className='footer-link' target='_blank' href='https://github.com/joyreacher/myFlix-client'>Github (Client)</a>
+            <a className='footer-link' target='_blank' href='https://github.com/joyreacher/movie_api'>Github (API)</a>
           </div>
         </Col>
         <Col className='footer__col d-flex flex-column align-items-sm-center pe-5' sm={6} md={6} lg={6}>
           <div>
             <h3 className='fs-3'>My Account</h3>
-            <Link to={path}>My Profile</Link>
+            <Link className='footer-link' target='_blank' to={path}>My Profile</Link>
           </div>
         </Col>
       </Container>
