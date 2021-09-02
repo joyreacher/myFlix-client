c9dc5e4699b52086bd55992f5da7a76885d6ab9b
Brian Thomas <btdeveloper125@gmail.com>
2021-09-02 11:36:37 -0600

    refactor: This commit upadates the card styles of the not featured

--- src/components/not-featured/not-featured.jsx
+++ src/components/not-featured/not-featured.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 // bootstrap
 import { Row, Col, Button, Image, Container, Card, CardColumns } from 'react-bootstrap'
+import './not-featured.scss'
 function NotFeatured ({ movies }) {
   return (
     <Row md={3} xl={4} className='gx-4 gx-lg-6 row-cols-2 justify-content-center'>{
@@ -9,22 +10,22 @@ function NotFeatured ({ movies }) {
         if (!movie.Featured) {
           return (
               <Col className='mb-5' key={`not-featured-${movie._id}`}>
-                <Card className='h-100 p-2'>
+                <Card className='shadow-1 h-100 p-2 main-bg'>
                   <Card.Img className='card-img-top' src={movie.ImagePath} />
                   <Card.Body className=''>
                     <Card.Text className="text-center fw-bolder text-truncate fs-6">
                       {movie.Description}
                     </Card.Text>
-                    <Link className="bg-dark" to={`/movies/${movie._id}`}>
-                      <Button>
+                    <Link to={`/movies/${movie._id}`}>
+                      <button className='shadow-1 btn btn-outline-dark'>
                         More
-                      </Button>
+                      </button>
                     </Link>
                   </Card.Body>
                   <Card.Footer>
                     <ul className='list-unstyled'>
-                      <li>Director: <p className='text-info'>{movie.Director.Name}</p></li>
-                      <li>Genre: <p className='text-info'>{movie.Genre.Name}</p></li>
+                      <li>Director: <p className='color-accent-2'>{movie.Director.Name}</p></li>
+                      <li>Genre: <p className='color-accent-2'>{movie.Genre.Name}</p></li>
                     </ul>
                   </Card.Footer>
                 </Card>
