33f7cb1b791ef611e1781b00b303dbff6b632e26
Brian Thomas <btdeveloper125@gmail.com>
2021-09-02 11:43:43 -0600

    refactor: This commit adds redux state to the genre view

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from 'react'
+import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 import Loading from '../loading-view/loading-view'
 // bootstrap
 import { Container, Row, Col, Button, Image } from 'react-bootstrap'
-export default function Genre ({ genre, onBackClick }) {
+import { loadGenre } from '../../actions/actions'
+const mapStateToProps = state => {
+  const { Genre } = state
+  return { Genre }
+}
+function Genre ({ genre, onBackClick, loadGenre, Genre }) {
   const [list, setList] = useState([])
   useEffect(() => {
     const accessToken = localStorage.getItem('token')
@@ -12,17 +18,22 @@ export default function Genre ({ genre, onBackClick }) {
       headers: { Authorization: `Bearer ${accessToken}` }
     }).then(res => {
       setList(res.data)
+      const data = res.data[0]
+      const list = res.data
+      console.log(data.Genre.Name, data.Genre.Description)
+      loadGenre(data.Genre.Name, data.Genre.Description)
       return list
     }).catch(function (error) {
       console.log(error)
     })
   }, [])
-  if (list.length === 0) return <Loading />
-  console.log(list)
+  if (!Genre) return <Loading />
+  console.log(Genre)
   return (
     <>
       <Container className='my-5'>
         <h1>All {genre}s</h1>
+        <p>{Genre.Description}</p>
         {
           list.map(movie => {
             return (
@@ -47,3 +58,4 @@ export default function Genre ({ genre, onBackClick }) {
     </>
   )
 }
+export default connect(mapStateToProps, { loadGenre })(Genre)
